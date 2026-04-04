b0812ed8b5a3b2ac5b4a7e37d19d99895f98746e
Tanishq Adate <adatetanishq1@gmail.com>
2026-04-04 14:02:41 +0530

    fix dependency conflicts

--- src/nlp_utils.py
+++ src/nlp_utils.py
@@ -7,6 +7,15 @@ spacy.cli.download("en_core_web_sm")
 
 def load_nlp():
     return spacy.load("en_core_web_sm")
+import spacy
+
+def load_nlp():
+    try:
+        return spacy.load("en_core_web_sm")
+    except:
+        import os
+        os.system("python -m spacy download en_core_web_sm")
+        return spacy.load("en_core_web_sm")
 
 def extract_persons(doc, is_valid_person):
     persons = []
